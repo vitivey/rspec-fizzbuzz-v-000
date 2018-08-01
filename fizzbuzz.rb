ccf1db35091fa30904f3d9113ef83b851bb0e333
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-01 21:53:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
+#require 'pry'
 
-def fizzbuz(number)
-  binding.pry
+def fizzbuzz(number)
+  #binding.pry
 
   print "20"
 
 end
-number=3
-fizzbuz(number)
