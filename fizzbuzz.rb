1dda95f4025e2925705e2d226bf515abd32b2112
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-01 21:51:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
+
 def fizzbuz(number)
+  binding.pry
+
+  print "20"
 
 end
+number=3
+fizzbuz(number)
